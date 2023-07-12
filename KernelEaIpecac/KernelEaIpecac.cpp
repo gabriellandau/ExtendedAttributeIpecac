50d3349e8fef632cb8b913b8486a4c697826e7b7
Gabriel Landau <42078554+gabriellandau@users.noreply.github.com>
2023-07-12 18:11:31 -0400

    Initial commit

---
+++ KernelEaIpecac/KernelEaIpecac.cpp
@@ -0,0 +1,157 @@
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <ktmw32.h>
+
+EXTERN_C
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+RtlAdjustPrivilege(
+    DWORD privilege,
+    bool bEnablePrivilege,
+    bool IsThreadPrivilege,
+    PBOOLEAN PreviousValue);
+
+typedef struct _REPARSE_DATA_BUFFER {
+    ULONG  ReparseTag;
+    USHORT ReparseDataLength;
+    USHORT Reserved;
+    union {
+        struct {
+            USHORT SubstituteNameOffset;
+            USHORT SubstituteNameLength;
+            USHORT PrintNameOffset;
+            USHORT PrintNameLength;
+            ULONG  Flags;
+            WCHAR  PathBuffer[1];
+        } SymbolicLinkReparseBuffer;
+        struct {
+            USHORT SubstituteNameOffset;
+            USHORT SubstituteNameLength;
+            USHORT PrintNameOffset;
+            USHORT PrintNameLength;
+            WCHAR  PathBuffer[1];
+        } MountPointReparseBuffer;
+        struct {
+            UCHAR DataBuffer[1];
+        } GenericReparseBuffer;
+    } DUMMYUNIONNAME;
+} REPARSE_DATA_BUFFER, * PREPARSE_DATA_BUFFER;
+
+bool
+RemoveKernelPurgeEAs(std::wstring filePath)
+{
+    bool bResult = false;
+    HANDLE hFile = INVALID_HANDLE_VALUE;
+    DWORD bytes = 0;
+    FILE_DISPOSITION_INFO dispInfo = { TRUE };
+    REPARSE_DATA_BUFFER reparse = {};
+    reparse.ReparseTag = IO_REPARSE_TAG_UNHANDLED;
+
+    filePath += L":RemoveKernelPurgeEAs";
+
+    HANDLE hTransaction = CreateTransaction(NULL, NULL, TRANSACTION_DO_NOT_PROMOTE, 0, 0, 0, NULL);
+    hFile = CreateFileTransactedW(filePath.c_str(), FILE_WRITE_ATTRIBUTES | DELETE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, nullptr, CREATE_ALWAYS,
+        FILE_FLAG_OPEN_REPARSE_POINT|FILE_FLAG_BACKUP_SEMANTICS, nullptr, hTransaction, NULL, NULL);
+    if (INVALID_HANDLE_VALUE == hFile)
+    {
+        printf(" [!] CreateFileTransactedW failed with GLE %u.  Retrying without TxF.\n", GetLastError());
+        CloseHandle(hTransaction);
+        hTransaction = NULL;
+
+        hFile = CreateFileW(filePath.c_str(), FILE_WRITE_ATTRIBUTES | DELETE,
+            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, nullptr, CREATE_ALWAYS,
+            FILE_FLAG_OPEN_REPARSE_POINT|FILE_FLAG_BACKUP_SEMANTICS, nullptr);
+        if (INVALID_HANDLE_VALUE == hFile)
+        {
+            printf(" [!] CreateFileW failed with GLE %u\n", GetLastError());
+            goto Cleanup;
+        }
+
+        printf(" [+] Created YOLO stream: %ws\n", filePath.c_str());
+    }
+    else
+    {
+        printf(" [+] Created stream in TxF: %ws\n", filePath.c_str());
+    }
+
+    if (!DeviceIoControl(hFile, FSCTL_SET_REPARSE_POINT, &reparse,
+        sizeof(reparse), nullptr, 0, &bytes, nullptr))
+    {
+        printf(" [!] DeviceIoControl(FSCTL_SET_REPARSE_POINT) failed with GLE %u\n", GetLastError());
+        goto Cleanup;
+    }
+    printf(" [+] Reparse point created.\n");
+
+    if (!DeviceIoControl(hFile, FSCTL_DELETE_REPARSE_POINT, &reparse,
+        sizeof(reparse), nullptr, 0, &bytes, nullptr))
+    {
+        printf(" [!] DeviceIoControl(FSCTL_DELETE_REPARSE_POINT) failed with GLE %u\n", GetLastError());
+        goto Cleanup;
+    }
+    printf(" [+] Reparse point removed.\n");
+
+    if (!SetFileInformationByHandle(hFile, FileDispositionInfo, &dispInfo, sizeof(dispInfo)))
+    {
+        printf(" [!] SetFileInformationByHandle(FileDispositionInfo) failed with GLE %u\n", GetLastError());
+        goto Cleanup;
+    }
+    printf(" [+] Stream removed.\n");
+
+    if (hTransaction)
+    {
+        CloseHandle(hFile); hFile = INVALID_HANDLE_VALUE;
+
+        if (!CommitTransaction(hTransaction))
+        {
+            printf(" [!] Failed to commit transaction.\n");
+        }
+        else
+        {
+            printf(" [+] Transaction committed.\n");
+        }
+        CloseHandle(hTransaction);
+    }
+    bResult = true;
+
+Cleanup:
+    if (INVALID_HANDLE_VALUE != hFile)
+    {
+        CloseHandle(hFile); hFile = INVALID_HANDLE_VALUE;
+    }
+    return bResult;
+}
+
+#define SE_BACKUP_PRIVILEGE (17L)
+#define SE_RESTORE_PRIVILEGE (18L)
+
+int wmain(int argc, wchar_t* argv[])
+{
+    bool bResult = false;
+    BOOLEAN ignored = FALSE;
+
+    if (argc < 2)
+    {
+        wprintf(L"Removes $Kernel.Purge EAs from the given file.\n\n");
+        wprintf(L"Usage: %s <FILE>\n", argv[0]);
+        return 0;
+    }
+
+    RtlAdjustPrivilege(SE_BACKUP_PRIVILEGE, TRUE, FALSE, &ignored);
+    RtlAdjustPrivilege(SE_RESTORE_PRIVILEGE, TRUE, FALSE, &ignored);
+
+    bResult = RemoveKernelPurgeEAs(argv[1]);
+    if (bResult)
+    {
+        printf(" [+] Operation successful.\n");
+    }
+    else
+    {
+        printf(" [!] Operation failed.\n");
+    }
+
+    return bResult ? 0 : 1;
+}
---
+++ README.md
@@ -0,0 +1,65 @@
+# Kernel EA Ipecac
+
+By [Gabriel Landau](https://twitter.com/GabrielLandau) @ [Elastic Security Labs](https://www.elastic.co/security-labs/).
+
+Removes [`$Kernel.Purge` Extended Attributes](https://learn.microsoft.com/en-us/windows-hardware/drivers/ifs/kernel-extended-attributes#auto-deletion-of-kernel-extended-attributes) from the given file.  Attempts to use TxF to reduce the consequence of failure, but falls back to YOLO mode.
+
+## Credit
+Idea courtesy of [@sixtyvividtails](https://twitter.com/sixtyvividtails) and inspired by [BBQ master @jonasLyk](https://twitter.com/jonasLyk) in this [conversation](https://twitter.com/GabrielLandau/status/1678813817545826305).
+
+## Example Usage
+
+```
+C:\Users\user\Desktop>fsutil file queryEA C:\Windows\System32\ntdll.dll
+
+Extended Attributes (EA) information for file C:\Windows\System32\ntdll.dll:
+
+Total Ea Size: 0x10b
+
+Ea Buffer Offset: 0
+Ea Name: $CI.CATALOGHINT
+Ea Value Length: 65
+0000:  01 00 61 00 4d 69 63 72  6f 73 6f 66 74 2d 57 69  ..a.Microsoft-Wi
+0010:  6e 64 6f 77 73 2d 43 6c  69 65 6e 74 2d 44 65 73  ndows-Client-Des
+0020:  6b 74 6f 70 2d 52 65 71  75 69 72 65 64 2d 50 61  ktop-Required-Pa
+0030:  63 6b 61 67 65 30 35 31  36 7e 33 31 62 66 33 38  ckage0516~31bf38
+0040:  35 36 61 64 33 36 34 65  33 35 7e 61 6d 64 36 34  56ad364e35~amd64
+0050:  7e 7e 31 30 2e 30 2e 32  32 36 32 31 2e 31 39 39  ~~10.0.22621.199
+0060:  32 2e 63 61 74                                    2.cat
+
+Ea Buffer Offset: 80
+Ea Name: $KERNEL.PURGE.ESBCACHE
+Ea Value Length: 6c
+0000:  6c 00 00 00 03 00 02 0c  95 8b 45 ad 5d 21 d9 01  l.........E.]!..
+0010:  80 65 80 f3 ae 35 d9 01  42 00 00 00 4e 00 27 01  .e...5..B...N.'.
+0020:  0c 80 00 00 20 3b d5 f1  a3 bf cc 98 c9 4e 5c 6f  .... ;.......N\o
+0030:  06 df c9 b4 e3 e3 47 94  b1 0a 1d 71 61 83 c2 bf  ......G....qa...
+0040:  38 1e 70 17 fa 27 00 0c  80 00 00 20 34 db f2 3f  8.p..'..... 4..?
+0050:  a4 a9 12 46 9a 99 26 89  00 46 44 7e 55 4b d7 44  ...F..&..FD~UK.D
+0060:  fa dc 41 ea 6c 16 92 fb  8b b6 6e b7              ..A.l.....n.
+
+C:\Users\user\Desktop>KernelEaIpecac.exe C:\Windows\System32\ntdll.dll
+ [+] Created stream in TxF: C:\Windows\System32\ntdll.dll:RemoveKernelPurgeEAs
+ [+] Reparse point created.
+ [+] Reparse point removed.
+ [+] Stream removed.
+ [+] Transaction committed.
+ [+] Operation successful.
+
+C:\Users\user\Desktop>fsutil file queryEA C:\Windows\System32\ntdll.dll
+
+Extended Attributes (EA) information for file C:\Windows\System32\ntdll.dll:
+
+Total Ea Size: 0x7d
+
+Ea Buffer Offset: 0
+Ea Name: $CI.CATALOGHINT
+Ea Value Length: 65
+0000:  01 00 61 00 4d 69 63 72  6f 73 6f 66 74 2d 57 69  ..a.Microsoft-Wi
+0010:  6e 64 6f 77 73 2d 43 6c  69 65 6e 74 2d 44 65 73  ndows-Client-Des
+0020:  6b 74 6f 70 2d 52 65 71  75 69 72 65 64 2d 50 61  ktop-Required-Pa
+0030:  63 6b 61 67 65 30 35 31  36 7e 33 31 62 66 33 38  ckage0516~31bf38
+0040:  35 36 61 64 33 36 34 65  33 35 7e 61 6d 64 36 34  56ad364e35~amd64
+0050:  7e 7e 31 30 2e 30 2e 32  32 36 32 31 2e 31 39 39  ~~10.0.22621.199
+0060:  32 2e 63 61 74                                    2.cat
+```
